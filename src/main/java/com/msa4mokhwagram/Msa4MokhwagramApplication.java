4c99e8b71089410c6e624328ffc03f5f6a2a86aa
dlaghxkr-oss <dlaghxkr@gmail.com>
2026-05-19 15:45:05 +0900

    260519 [develop] init

---
+++ src/main/java/com/msa4mokhwagram/Msa4MokhwagramApplication.java
@@ -0,0 +1,13 @@
+package com.msa4mokhwagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msa4MokhwagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Msa4MokhwagramApplication.class, args);
+    }
+
+}
